7bcb7c3e75b83fb4c3a754375da5c2c604f7a74a
sanicodeplayground <magiur@gmail.com>
2024-08-27 17:36:17 +0100

    add content

---
+++ src/app/experiences.js
@@ -0,0 +1,22 @@
+const experiences = [
+  {
+    role: "Product Designer at Global App Testing",
+    time: "October 2021 - April 2024",
+    tags: ["B2C", "B2B", "Software"],
+  },
+  {
+    role: "Product Designer at Global App Testing",
+    time: "October 2021 - April 2024",
+    tags: ["B2C", "B2B", "Software"],
+  },
+  {
+    role: "Product Designer at Global App Testing",
+    time: "October 2021 - April 2024",
+    tags: ["B2C", "B2B", "Software"],
+  },
+  {
+    role: "Product Designer at Global App Testing",
+    time: "October 2021 - April 2024",
+    tags: ["B2C", "B2B", "Software"],
+  },
+];
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const ibmPlexSans = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Rosina Pissaco",
@@ -17,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} antialiased max-w-3xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto`}
+        className={`${ibmPlexSans.className} antialiased max-w-3xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto`}
       >
         {children}
       </body>
