d1a02791dfe3333c3919bbe21e2aa39877b551d4
sanicodeplayground <magiur@gmail.com>
2024-08-28 16:01:00 +0100

    add single project

---
+++ src/app/projects.ts
@@ -0,0 +1,27 @@
+const projects = [
+  {
+    title: "Testing dashboard powered by AI",
+    page: "dw.com",
+    tags: ["B2C", "B2B", "Software QA"],
+    img: "/portfolio_thumbnail/thumbnail_01.png",
+  },
+  {
+    title: "Your personalised cycling coach app",
+    page: "dw.com",
+    tags: ["B2C", "Mobile App", "Fitness & Health"],
+    img: "/portfolio_thumbnail/thumbnail_02.jpg",
+  },
+  {
+    title: "Music clips for messages",
+    page: "dw.com",
+    tags: ["B2C", "Mobile", "Social"],
+    img: "/portfolio_thumbnail/thumbnail_03.jpg",
+  },
+  {
+    title: "A Syndicate Lottery App",
+    page: "dw.com",
+    tags: ["B2C", "Mobile", "Ecommerce"],
+    img: "/portfolio_thumbnail/thumbnail_04.jpg",
+  },
+];
+export default projects;
