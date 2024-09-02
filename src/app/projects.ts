9ee997f730d8bee20b8f807cd1639b2d85cb61ce
sanicodeplayground <magiur@gmail.com>
2024-09-02 13:27:04 +0100

    invert logo remove

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -3,25 +3,25 @@ const projects = [
     title: "Testing dashboard powered by AI",
     page: "/spoked/",
     tags: ["B2C", "B2B", "Software QA"],
-    img: "/portfolio-thumbnail/thumbnail01.png",
+    img: "/portfolio-thumbnail/thumbnail-gat.png",
   },
   {
     title: "Your personalised cycling coach app",
     page: "/spoked/",
     tags: ["B2C", "Mobile App", "Fitness & Health"],
-    img: "/portfolio-thumbnail/thumbnail02.jpg",
+    img: "/portfolio-thumbnail/thumbnail-spoked.png",
   },
   {
     title: "Music clips for messages",
     page: "/spoked/",
     tags: ["B2C", "Mobile", "Social"],
-    img: "/portfolio-thumbnail/thumbnail03.jpg",
+    img: "/portfolio-thumbnail/thumbnail-songclip.png",
   },
   {
     title: "A Syndicate Lottery App",
     page: "/spoked/",
     tags: ["B2C", "Mobile", "Ecommerce"],
-    img: "/portfolio-thumbnail/thumbnail04.jpg",
+    img: "/portfolio-thumbnail/thumbnail-hatchster.png",
   },
 ];
 export default projects;
