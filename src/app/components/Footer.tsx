b5ad8ca34ba69c36faaa8ae36c522fba4f3b060c
sanicodeplayground <magiur@gmail.com>
2024-10-07 13:08:57 +0100

    fix footer link

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,34 +1,44 @@
-import CircleHighlight from "./CircleHighlight";
-import {
-  FaRegEnvelope,
-  FaLinkedinIn,
-  FaDribbble,
-  FaBehance,
-} from "react-icons/fa";
+"use client";
+
+import { FaRegEnvelope, FaLinkedinIn, FaDribbble } from "react-icons/fa";
 
 export default function Footer() {
+  const encodedEmail = "rosinaaa[at]gmail[dot]com";
+
+  const decodeEmail = () => {
+    return encodedEmail.replace("[at]", "@").replace("[dot]", ".");
+  };
+
   return (
     <section className="my-grid text-center mt-2">
       <hr />
       <p className="text-lg font-bold mb-6">
-        Thank you for your interest in my work.{" "}
-        <CircleHighlight>Lets connect!</CircleHighlight>
+        Thank you for your interest in my work. Lets connect!
       </p>
       <nav aria-label="Social media links">
         <ul className="flex justify-center space-x-6">
           <li>
-            <a href="mailto:rosieintech@gmails.com" aria-label="Email">
-              <FaRegEnvelope className="size-6" />
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                window.location.href = `mailto:${decodeEmail()}`;
+              }}
+              aria-label="Email"
+              className="group"
+            >
+              <FaRegEnvelope className="size-6 group-hover:scale-110 transition-all duration-300 ease-in-out" />
             </a>
           </li>
           <li>
             <a
-              href="https://www.linkedin.com/in/rosinapissaco/"
+              href="https://www.linkedin.com/in/rosieprofile/"
               target="_blank"
               rel="noopener noreferrer"
               aria-label="LinkedIn"
+              className="group"
             >
-              <FaLinkedinIn className="size-6" />
+              <FaLinkedinIn className="size-6 group-hover:scale-110 transition-all duration-300 ease-in-out" />
             </a>
           </li>
           <li>
@@ -37,18 +47,9 @@ export default function Footer() {
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Dribbble"
+              className="group"
             >
-              <FaDribbble className="size-6" />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.behance.net/rosinapissaco"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Behance"
-            >
-              <FaBehance className="size-6" />
+              <FaDribbble className="size-6 group-hover:scale-110 transition-all duration-300 ease-in-out" />
             </a>
           </li>
         </ul>
